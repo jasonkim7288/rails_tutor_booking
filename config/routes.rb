fe9d9ad844056ea18b880ed7618277026cceb323
jasonkim7288 <jason.kim7288@gmail.com>
2020-07-28 18:52:37 +1000

    add select tag on index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'profiles/:id/edit', to: 'profiles#edit', as: 'edit_profile'
   get 'settings/notifications'
   get 'settings/close_account'
-  get 'tutor_sessions/search', to: 'tutor_sessions#search', as: 'search_tutor_session'
+  get 'tutor_sessions/search_result', to: 'tutor_sessions#search_result', as: 'search_tutor_session'
   resources :tutor_sessions
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'tutor_sessions#index'
